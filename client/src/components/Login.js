84b8464feeb762f52264908eed82180961cc61ab
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2019-04-18 15:18:37 -0700

    added Grid

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,9 @@
 import React,  { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
 import TopBar from './TopBar';
 import LoginForm from './Login_Components/LoginForm'
 import RegisterForm from './Login_Components/RegisterForm'

b32fc013c95b7456d919365e984b062ec5855bae
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2019-04-19 21:27:19 -0700

    Added Mui Theme, set page layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Routes from './routes'
 import './App.css';
+import { MuiThemeProvider,createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      //light: '#757ce8',
+      main: '#4490e6',
+      //dark: '#002884',
+      contrastText: '#fff',
+    },
+    // secondary: {
+    //   light: '#ff7961',
+    //   main: '#f44336',
+    //   dark: '#ba000d',
+    //   contrastText: '#000',
+    // },
+  },
+  typography:{
+    fontFamily:['Raleway']
+  }
+});
 
 
 class App extends Component {
   render(){
     return(
-      <div>
-      <Routes />
-    </div>
+      <MuiThemeProvider theme={theme}>
+        <Routes />
+      </MuiThemeProvider>
     );
   }
 }
--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -3,22 +3,19 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import logo from '../assets/MapSpace.png';
+import '../App.css';
 
 const styles = {
   root: {
     flexGrow: 1,
   },
   grow: {
-    flexGrow: 1,
+
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+
 };
 
 function TopBar(props){
@@ -27,13 +24,10 @@ function TopBar(props){
         <div className={classes.root}>
           <AppBar position="static">
             <Toolbar>
-
-              <Typography variant="h6" color="inherit" className={classes.grow}>
-                MapSpace
-              </Typography>
-              <Typography variant="h6" color="inherit" className={classes.grow}>
+              <img src={logo} className="logo" alt="MapSpace" />
+              <h4>
                 Trending
-              </Typography>
+              </h4> 
 
             </Toolbar>
           </AppBar>

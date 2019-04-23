a38e0d83ee08d718f597841024774c1a7ec27488
Sunny Wong <swong6@mail.sfsu.edu>
2019-04-23 16:33:50 -0700

    Search page in progress

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -3,19 +3,66 @@ import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
-import LoginForm from './Login_Components/LoginForm';
-import RegisterForm from './Login_Components/RegisterForm';
 import Typography from '@material-ui/core/Typography';
-import '../App.css';
 import { CardContent } from '@material-ui/core';
+import '../App.css';
+import './SearchPage.css';
+import LoggedInTopBar from './LoggedInTopBar';
+
+
 
 class SearchPage extends Component{
+
+  state = {
+    search: '',
+  };
+
+  handleChange = prop => event => {
+    this.setState({ [prop]: event.target.value });
+  };
 
   render(){
     return(
       <div>
-        
+        <LoggedInTopBar />
+        <Card>
+            <CardContent>
+            <Typography className="findFriends" variant="h5">Find Friends</Typography>
+
+             {/* Search Field */}
+             <TextField
+              id="search"
+              className="searchField"
+              variant="outlined"
+              type="text" 
+              value={this.state.username}
+              onChange={this.handleChange('search')}
+              placeholder="Search Username"
+              InputProps={{
+                startAdornment: <InputAdornment position="start">@</InputAdornment>,
+              }}
+            />
+             <br/><br/>
+              {/* Finding Friends */}
+              <Card>
+                <Typography className="userNames" variant="h5">@Username
+
+                  <Button className="followButton">
+                   +Follow
+                  </Button>
+
+                  <Button className ="viewProfileButton" href="#">
+                  View Profile
+                  </Button>
+                </Typography>  
+              </Card>
+
+            </CardContent>
+            <br/><br/>
+        </Card>
 
 
       </div>

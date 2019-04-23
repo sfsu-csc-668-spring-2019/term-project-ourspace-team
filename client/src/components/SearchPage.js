4b746a20307882c1288203010d727fdf43ef1757
Sunny Wong <swong6@mail.sfsu.edu>
2019-04-23 15:37:43 -0700

    Added empty search page to routes

---
+++ client/src/components/SearchPage.js
@@ -0,0 +1,26 @@
+import React,  { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Card from '@material-ui/core/Card';
+import LoginForm from './Login_Components/LoginForm';
+import RegisterForm from './Login_Components/RegisterForm';
+import Typography from '@material-ui/core/Typography';
+import '../App.css';
+import { CardContent } from '@material-ui/core';
+
+class SearchPage extends Component{
+
+  render(){
+    return(
+      <div>
+        
+
+
+      </div>
+    );
+  }
+}
+
+export default SearchPage;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Landing from './components/Landing'
+import SearchPage from './components/SearchPage'
 
 export default props => (
     <BrowserRouter>
         <Switch>
           <Route exact path='/' component={ Landing} />
           <Route exact path='/login' component={ Landing } />
+          <Route exact path='/search' component={ SearchPage } />
+
 
         </Switch>
     </BrowserRouter>

3075ccb23806ef38058667baf3d1301cdd9e00fa
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2019-04-23 11:13:28 -0700

    Added HomePage file and LoggedInNav files

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Landing from './components/Landing'
+import Home from './components/HomePage'
 
 export default props => (
     <BrowserRouter>
         <Switch>
           <Route exact path='/' component={ Landing} />
           <Route exact path='/login' component={ Landing } />
+          <Route exact path='/home' component={Home}/>
 
         </Switch>
     </BrowserRouter>

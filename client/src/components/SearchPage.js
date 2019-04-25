91b5d2409053970b363a67fe6d3f9c34cd862663
Sunny Wong <swong6@mail.sfsu.edu>
2019-04-24 23:57:11 -0700

    Finished search page

--- client/src/components/SearchPage.js
+++ client/src/components/SearchPage.js
@@ -1,72 +1,104 @@
-import React,  { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import CardContent from '@material-ui/core/CardContent';
+import SearchResults from './SearchResults'
 import '../App.css';
 import './SearchPage.css';
 import LoggedInTopBar from './LoggedInTopBar';
 
 
-class SearchPage extends Component{
+const styles = {
+  viewProfileButton:{
+    background: '#83bc45',
+    color: '#fff',
+    marginRight: 15,
+    float: 'right',
+    textTransform: 'none',
+  },
+  followButton:{
+    background: '#4490e6',
+    color: '#fff',
+    fontFamily: 'Raleway',
+    float: 'right',
+    marginRight: 15,
+    textTransform: 'none',
+  },
+  AccountCircleIcon:{
+    float: 'left',
+    paddingTop: 5,
+    paddingRight: 25,
+    paddingLeft: 40,
+    
+  }
+};
+
+
+class SearchPage extends Component {
 
   state = {
-    search: '',
+    search: ''
   };
 
   handleChange = prop => event => {
     this.setState({ [prop]: event.target.value });
   };
 
-  render(){
-    return(
+  render() {
+    
+    return (
       <div>
         <LoggedInTopBar />
-        <Card className="card outerCardStyle">
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing="40"
+        >
+          <Card className="card outerCardStyle">
             <CardContent>
-
-            <Typography className="findFriends" variant="h6">Find Friends</Typography>
-             {/* Search Field */}
-             <TextField
-              className="searchField"
-              id="search"
-              variant="outlined"
-              type="text" 
-              value={this.state.search}
-              onChange={this.handleChange('search')}
-              placeholder="Search Username"
-              InputProps={{
-                startAdornment: <InputAdornment position="start">@</InputAdornment>,
-              }}
-            />
-             <br/><br/>
-              {/* Finding Friends */}
-              <Card className="innerCardStyle">
-                <Typography className="userNames" variant="h5">@Username
-
-                  <Button className="followButton">
-                   +Follow
-                  </Button>
-
-                  <Button className ="viewProfileButton" href="#">
-                  View Profile
-                  </Button>
-                </Typography>  
-              </Card>
-
+              <Typography className="findFriends" variant="h6">Find Friends</Typography>
+              {/* Search Field */}
+              <Grid
+                className="searchFieldGrid"
+                item xs
+                direction="row"
+                justify="flex-start"
+                alignItems="center"
+              >
+                <TextField
+                  className="searchField"
+                  id="search"
+                  variant="outlined"
+                  type="text"
+                  value={this.state.search}
+                  onChange={this.handleChange('search')}
+                  placeholder="Search Username"
+                  InputProps={{
+                    startAdornment: <InputAdornment position="start">@</InputAdornment>,
+                  }}
+                />
+                <SearchResults />
+              </Grid>
             </CardContent>
-            <br/><br/>
-        </Card>
-
-
+          </Card>
+        </Grid>
       </div>
     );
   }
 }
 
-export default SearchPage;
+SearchPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SearchPage);
---
+++ client/src/components/SearchResults.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import CardContent from '@material-ui/core/CardContent';
+import './SearchPage.css';
+
+const styles = {
+    viewProfileButton:{
+      background: '#83bc45',
+      color: '#fff',
+      marginRight: 15,
+      float: 'right',
+      textTransform: 'none',
+    },
+    followButton:{
+      background: '#4490e6',
+      color: '#fff',
+      fontFamily: 'Raleway',
+      float: 'right',
+      marginRight: 15,
+      textTransform: 'none',
+    },
+    AccountCircleIcon:{
+      float: 'left',
+      paddingTop: 5,
+      paddingRight: 25,
+      paddingLeft: 40,
+      
+    }
+  };
+class SearchResults extends Component {
+    state = {
+        search: '',
+        result:''
+      };
+
+      handleChange = prop => event => {
+        this.setState({ [prop]: event.target.value });
+      };
+
+    render() {
+        const { classes } = this.props;
+        return (
+                <Grid className="innerCardGrid">
+                <Card className="innerCardStyle">
+                  <Typography className="userNames" 
+                    variant="h6" 
+                    value={this.state.result}
+                    onChange={this.handleChange('results')}
+                  > 
+                    @  {this.state.result}
+                    <AccountCircle className={classes.AccountCircleIcon} />
+                    <Button className={classes.viewProfileButton} href="#">View Profile</Button>
+                    <Button className={classes.followButton}> + Follow</Button>
+                  </Typography>
+                  </Card>
+                </Grid>
+        );
+    }
+}
+
+SearchResults.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(SearchResults);

6dcf6937b10165eb70d967dd723ec475630bb769
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2019-04-25 11:11:03 -0700

    placed Map compnent on the home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import LoggedInTopBar from './LoggedInTopBar';
+import Typography from '@material-ui/core/Typography';
+import Map from './Map_Components/Map';
 import '../App.css';
 import { CardContent } from '@material-ui/core';
 
@@ -11,8 +13,22 @@ class HomePage extends Component {
         return (
             <div>
                 <LoggedInTopBar />
-                <Grid container spacing={24}>
 
+                <Grid container spacing={24}>
+                
+                    <Grid container item xs={12} direction="row" spacing={24}>
+                        <Grid item xs={4}>
+                        <Card>
+                            <CardContent>
+                            <Typography variant="title">Welcome to MapSpace</Typography>
+
+                                </CardContent>
+                        </Card>
+                        </Grid>
+                        <Grid item xs={8} container direction="column">
+                            <Map />
+                        </Grid>
+                    </Grid>
                 </Grid>
             </div>
         );

744f7cb025514d12f19048ac20ea0aa776aa46fc
Tommy Tran <tmytrn@Tommys-MacBook-Pro-2.local>
2019-04-27 13:25:05 -0700

    fixed account popover, layout tweaks

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
-import Card from '@material-ui/core/Card';
 import LoggedInTopBar from './LoggedInTopBar';
-import Typography from '@material-ui/core/Typography';
-import ListItem from '@material-ui/core/ListItem';
-import List from '@material-ui/core/List';
-import ListItemText from '@material-ui/core/ListItemText';
+import ProfileCard from './Home_Components/ProfileCard';
 import Map from './Map_Components/Map';
 import '../App.css';
 import { CardContent } from '@material-ui/core';
+import PlacesList from './Home_Components/PlacesList';
 
-function generate(element) {
-    return [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(value =>
-        React.cloneElement(element, {
-            key: value,
-        }),
-    );
-}
+
 
 class HomePage extends Component {
 
@@ -24,48 +15,22 @@ class HomePage extends Component {
         return (
             <div>
                 <LoggedInTopBar />
-
                 <div>
-                <Grid container spacing={24}>
-
-                    <Grid container item xs={12} direction="row" spacing={24}>
-                        <Grid container direction="row" item xs={4} spacing={12}>
-                            <Grid item xs={12}>
-                                <div>
-
-                                    <Card>
-                                        <CardContent>
-                                            <Typography variant="title">Welcome to MapSpace</Typography>
-
-                                        </CardContent>
-                                    </Card>
-                                </div>
+                    <Grid container spacing={24}>
+                        <Grid container item sm={12} direction="row" spacing={24}>
+                            <Grid container direction="column" item sm={4} spacing={8}>
+                                <Grid item sm>
+                                  <ProfileCard/>
+                                </Grid>
+                                <Grid item sm>
+                                  <PlacesList/>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <Card>
-                                    <CardContent>
-                                        <div >
-                                            <List >
-                                                {generate(
-                                                    <ListItem>
-                                                        <ListItemText
-                                                            primary="Single-line item"
-                                                        />
-                                                    </ListItem>,
-                                                )}
-                                            </List>
-                                        </div>
-                                    </CardContent>
-                                </Card>
+                            <Grid item xs={8} container direction="column">
+                                <Map />
                             </Grid>
-
-
-                        </Grid>
-                        <Grid item xs={8} container direction="column">
-                            <Map />
                         </Grid>
                     </Grid>
-                </Grid>
                 </div>
             </div>
         );
---
+++ client/src/components/Home_Components/ProfileCard.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Gravatar from 'react-gravatar'
+import Avatar from '@material-ui/core/Avatar';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    marginTop: '1rem',
+    marginLeft: '1rem',
+    maxWidth: 500,
+  },
+  image: {
+    width: 128,
+    height: 128,
+  },
+  img: {
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+  },
+  avatar:{
+    margin: 'auto',
+    display: 'block',
+    width: 75,
+    height: 75,
+  }
+});
+
+class ProfileCard extends React.Component {
+
+  //  gravatarUrl(email) {
+  //   const lowerCaseEmail = email.toLowerCase()
+  //   return `http://www.gravatar.com/avatar/${md5(lowerCaseEmail)}`;
+  // }
+
+  // const Gravatar = ({email}) => <img src={gravatarUrl(email)} />;
+
+  // Gravatar.propTypes = {
+  //   email: React.PropTypes.string.isRequired
+  // };
+  
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <Grid container spacing={16}>
+            <Grid item>
+              <ButtonBase className={classes.image}>
+              <Avatar className ={classes.avatar} src="http://www.gravatar.com/avatar/"/>
+
+              </ButtonBase>
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={16}>
+                <Grid item xs>
+                  <Typography gutterBottom variant="subtitle1">
+                    @Username
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
+    );
+
+  }
+}
+ProfileCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(ProfileCard);

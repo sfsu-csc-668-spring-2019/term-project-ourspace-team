ba52835edaa3b206858ca18a81271c815d00dd07
Sunny Wong <swong6@mail.sfsu.edu>
2019-04-29 16:15:56 -0700

    Profile page finished

--- client/src/components/ProfilePage_Components/UserContent.js
+++ client/src/components/ProfilePage_Components/UserContent.js
@@ -5,14 +5,30 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    text:{
+      float: 'left',
+      fontSize: "20px",
+      paddingRight: 25,
+      paddingLeft: 40,
+      paddingBottom: 50,
+    },
+    };
+
 class UserContent extends Component{
     render(){
+        const { classes } = this.props;
         return(
             <div>
-                <Typography>@ username</Typography>
+                <Typography className={classes.text}>{this.props.usertag} saved their favorite donut shop!</Typography>
             </div>
         );
     }
 }
 
-export default UserContent;
+UserContent.propTypes = {
+    classes: PropTypes.object.isRequired,
+  };
+  
+  export default withStyles(styles)(UserContent);
--- client/src/components/ProfilePage_Components/UserFeed.js
+++ client/src/components/ProfilePage_Components/UserFeed.js
@@ -6,8 +6,6 @@ import Card from '@material-ui/core/Card';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import { typography } from '@material-ui/system';
-import { unstable_Box as Box } from '@material-ui/core/Box';
 import './UserFeed.css';
 import UserContent from './UserContent';
 
@@ -23,35 +21,35 @@ const styles = {
     followButton:{
         background: '#4490e6',
         float: 'right',
-        marginRight: 100,
-        marginTop: 100,
-        paddingLeft: 150,
-        paddingRight: 150,
+        marginTop: 40,
+        marginLeft: 100,
+        marginRight: 40,
+        paddingLeft: 170,
+        paddingRight: 170,
 
     },
     userName:{
+        useNextVariants: true,
         float: 'left',
         fontSize: "25px",
         paddingTop: 10,
       },
     root:{
+        useNextVariants: true,
         float: 'right',
         fontSize: "20px",
-        paddingRight: 20,
+        marginTop: 10,
+        paddingRight: 45,
       },
-    values:{
-        float: 'right',
-        fontSize: "20px",
-        paddingRight: 20,
-    }
+
   };
 
 class UserFeed extends Component {
     state = {
-        userName: '',
+        userName: 'Donut',
         following: 0,
         followers: 0,
-        placesSaved: 0
+        placesSaved: 1
       };
 
     render() {
@@ -61,16 +59,23 @@ class UserFeed extends Component {
                <Card className="outerCard">
                     <CardContent className="outerCardContents">
                         <AccountCircle className={classes.AccountCircleIcon} />
-                        <Typography className={classes.userName} value={this.state.userName}> @ username</Typography>
-                        <Typography className="savedPlaces" value={this.state.placesSaved}>
-                            <Box>Places Saved</Box>
+                        <Typography className={classes.userName} value={this.state.userName}> @ {this.state.userName}</Typography>
+                        {/*Follower, Following, Places Saved count*/}
+                        <Typography className={classes.root} value={this.state.placesSaved}>
+                            <Typography component ="span" className="placesSavedValues">{this.state.placesSaved} </Typography>
+                            Places Saved
                         </Typography>
-                        <Typography className={classes.root} value={this.state.followers}><div>{this.state.followers}</div>Followers</Typography>
-                        <Typography className={classes.root} value={this.state.following}><div>{this.state.following}</div>Following</Typography>
+                        <Typography className={classes.root} value={this.state.followers}>
+                            <Typography component ="span" className="followerValues">{this.state.followers} </Typography>
+                            Followers
+                        </Typography>
+                        <Typography className={classes.root} value={this.state.following}>
+                            <Typography component ="span" className="followingValues">{this.state.following} </Typography>
+                            Following
+                        </Typography>
                         <Button className={classes.followButton}>Follow</Button>
-
                         <Card className="innerCardContents">
-                            <UserContent />
+                            <UserContent usertag={this.state.userName}/>
                         </Card>
                         
                     </CardContent>

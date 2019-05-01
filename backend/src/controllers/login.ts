62539203469e0dcd331d68105cc62f949d798b6b
unknown <jdvaldez@mail.sfsu.edu>
2019-05-01 16:40:55 -0700

    Reconfigured login.ts file into a more object orientative way similar to registration

--- backend/src/controllers/login.ts
+++ backend/src/controllers/login.ts
@@ -5,34 +5,36 @@ import * as bcrypt from 'bcryptjs';
 
 import "reflect-metadata";
 
-export let tryToLogin = async (req: Request, res: Response, next: NextFunction) => {
-    let userRepo: UserRepo = new UserRepo();
-    const userName: string = req.body.username;
-    const userEmail: string = req.body.email;
-    const passWord: any = req.body.password;
-    const name: string = req.body.name;
+export class LoginObj {
 
-    console.log("Try to login");
-    const user = await userRepo.doesUserAlreadyExist(userName, userEmail);
+    async tryToLogin(req: Request, res: Response, next: NextFunction) {
+        let userRepo: UserRepo = new UserRepo();
+        const userName: string = req.body.username;
+        const userEmail: string = req.body.email;
+        const passWord: any = req.body.password;
+        const name: string = req.body.name;
 
-    if (user.length != 0) {
-        console.log('User exists');
-        if (userName === user[0].username) {
-            const match = await bcrypt.compare(passWord, user[0].password);
-            console.log(match);
-            if (match === true) {
-                //response.send('logged in');
-                console.log('logged in');
+        console.log("Try to login");
+        const user = await userRepo.doesUserAlreadyExist(userName, userEmail);
+
+        if (user.length != 0) {
+            console.log('User exists');
+            if (userName === user[0].username) {
+                const match = await bcrypt.compare(passWord, user[0].password);
+                console.log(match);
+                if (match === true) {
+                    //response.send('logged in');
+                    console.log('logged in');
+                }
+
+                if (!match) {
+                    //response.send('login failed');
+                    console.log('login failed');
+                }
             }
-
-            if (!match) {
-                //response.send('login failed');
-                console.log('login failed');
-            }
+        } else {
+            //response.send('User not found');
+            console.log('User not found');
         }
-    } else {
-        //response.send('User not found');
-        console.log('User not found');
     }
-
 }

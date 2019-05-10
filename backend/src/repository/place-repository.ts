fa5211a7b93f4949b2c1c3a34f86a127b98744f6
Janda95 (Mars) <j.rettinghouse@gmail.com>
2019-05-10 16:47:31 -0700

    place repository added

---
+++ backend/src/repository/place-repository.ts
@@ -0,0 +1,10 @@
+import { Place } from "../entity/PlaceEntity";
+import { json } from "body-parser";
+ 
+export class PlaceRepo {
+    saveMap(place: Place){
+        console.log(place);
+        return
+    }
+
+}

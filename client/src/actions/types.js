0a6452c272c5c469f1f47d409612055a60e054e1
Robert <rquinones93@yahoo.com>
2019-05-10 11:47:03 -0700

    Added reducer index file, actions types file

---
+++ client/src/actions/types.js
@@ -0,0 +1,2 @@
+// Constant names for different reducers
+export const SHOW_COMMENTS = 'SHOW_COMMENTS'
---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+// Import Reducers Here
+
+export default combineReducers({
+  // Name of Reducer to be Exported : Name of Reducer Imported
+});

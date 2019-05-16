cbadd524936ff862c816d0ccdcc2689dd6ae745b
Janda95 (Mars) <j.rettinghouse@gmail.com>
2019-05-16 13:09:13 -0700

    added dummy data and relations

--- backend/src/controllers/homepage.ts
+++ backend/src/controllers/homepage.ts
@@ -1,5 +1,11 @@
 import { Response, Request, NextFunction} from "express";
 
+import { User } from "../entity/UserEntity";
+import { Place } from "../entity/PlaceEntity";
+import { Comment } from "../entity/CommentEntity";
+import { Map } from "../entity/MapEntity";
+
+
 export class HomepageController {
 
     //get "/""
@@ -16,8 +22,42 @@ export class HomepageController {
         res.send("Index Page Hit");
     }
 
-    async createTablesWithDummyData (req: Request, res: Response, next: NextFunction){
-        
+    async createTablesWithDummyData(req: Request, res: Response, next: NextFunction){
+        //Create basic entities
+        const dummyUser: User = new User();
+        dummyUser.name = "John";
+        dummyUser.username = "IamJohn"
+        dummyUser.password = "NonHashedPassword"
+        dummyUser.email = "IamJohn@gmail.com"
+        await User.save(dummyUser);
+
+        const dummyMap: Map = new Map();
+        await Map.save(dummyMap);
+
+        const dummyPlace: Place = new Place();
+        dummyPlace.place_id
+        await Place.save(dummyPlace);
+
+        const dummyComment: Comment = new Comment();
+        dummyComment.description = "This is a comment contents example.";
+        await Comment.save(dummyComment);
+
+        //Connect Relations with created saved entities
+        dummyUser.maps = [dummyMap];
+        dummyUser.comments = [dummyComment];
+        await User.save(dummyUser);
+
+        dummyMap.places = [dummyPlace];
+        await Map.save(dummyMap);
+
+        dummyPlace.comments = [dummyComment];
+        await Place.save(dummyPlace);
+
+
+        //Remove Entities after created
+
+
+
         res.send("Created tables with data");
     }
 
